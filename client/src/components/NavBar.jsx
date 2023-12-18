22412954a73aecf78e39a9948dc6555a1dc20a24
ydk1204 <ejrbdi@naver.com>
2023-12-19 00:45:09 +0900

    login and register page style

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -2,11 +2,24 @@ import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <div className="w-full h-14 mb-4 bg-black text-3xl">
+    <div className="w-full h-14 mb-4 bg-black px-40">
       <div className="w-full h-full flex items-center justify-between">
-        <h2>
-          <Link to="/">Chat App</Link>
+        <h2 className="text-3xl">
+          <Link to="/" className="no-underline">
+            Chat App
+          </Link>
         </h2>
+        <span className="text-yellow-500">Logged in as YDKs</span>
+        <nav className="w-40">
+          <ul className="flex text-base justify-between">
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   );
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,5 +1,42 @@
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <>
+      <div className="flex justify-center items-center">
+        <form
+          action=""
+          className="flex flex-col h-screen pt-[10%] w-[50vw] text-black"
+        >
+          <div className="w-full text-start mb-10">
+            <h2 className="text-5xl text-white">Register</h2>
+          </div>
+          <input
+            className="mb-4 h-10 rounded-xl p-5"
+            type="text"
+            placeholder="Name"
+          />
+          <input
+            className="mb-4 h-10 rounded-xl p-5"
+            type="email"
+            placeholder="Email"
+          />
+          <input
+            className="mb-4 h-10 rounded-xl p-5"
+            type="password"
+            placeholder="Password"
+          />
+          <button
+            className="mb-4 h-12 rounded-xl text-xl bg-blue-600 text-white"
+            type="submit"
+          >
+            Register
+          </button>
+          <span className="h-24 bg-red-200 text-red-700 p-5 rounded-xl">
+            An Error occured
+          </span>
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default Register;
